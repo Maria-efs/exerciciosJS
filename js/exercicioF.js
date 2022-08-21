80c6c5ed50acb265f8d17b9bc7319284f4530af3
Maria-efs <mfs.6401131@gmail.com>
2022-08-21 13:03:37 -0300

    atualizado

--- js/exercicioF.js
+++ js/exercicioF.js
@@ -1,10 +1,25 @@
 'use strict'
 
-function ordemCrescente(){
+function ordemCrescente() { 
     const number1 = document.getElementById('number1').value;
     const number2 = document.getElementById('number2').value;
     const number3 = document.getElementById('number3').value;
-    const resultado = document.getElementById('resultado');
-}
+    const resultado = document.getElementById('resultado')
 
-calcular.addEventListener('click', ordemCrescente)
+    let maior, menor, meio
+
+    if(number1 > number2 && number1 > number3){
+        maior = number1
+    }
+    else if (number1 < number2 && number1 < number3){
+        menor = number1
+    }else{
+        meio = number1
+    }
+
+    resultado.textContent = `${menor} ${meio} ${maior}`
+
+}
+    
+ordenar.addEventListener('click', ordemCrescente)
+
