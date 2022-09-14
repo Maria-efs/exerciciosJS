1a9b0bc7ff2a1c5e7d940f9920c257c5922b1b2a
Maria-efs <98433714+Maria-efs@users.noreply.github.com>
2022-09-14 08:21:05 -0300

    atualizado 14/09

--- js/exercicioF.js
+++ js/exercicioF.js
@@ -1,25 +1,16 @@
 'use strict'
 
-function ordemCrescente() { 
-    const number1 = document.getElementById('number1').value;
-    const number2 = document.getElementById('number2').value;
-    const number3 = document.getElementById('number3').value;
-    const resultado = document.getElementById('resultado')
+const gerenciarNumeros = function () {
+    const number1 = +document.getElementById('number1').value;
+    const number2 = +document.getElementById('number2').value;
+    const number3 = +document.getElementById('number3').value;
+    const resultado = document.getElementById('resultado');
 
-    let maior, menor, meio
+    let valor = [number1, number2, number3];
+    valor.sort((a, b) => a - b);
 
-    if(number1 > number2 && number1 > number3){
-        maior = number1
-    }
-    else if (number1 < number2 && number1 < number3){
-        menor = number1
-    }else{
-        meio = number1
-    }
-
-    resultado.textContent = `${menor} ${meio} ${maior}`
-
+    resultado.textContent = valor;
 }
-    
-ordenar.addEventListener('click', ordemCrescente)
+
+ordenar.addEventListener('click', gerenciarNumeros);
 
